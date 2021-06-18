a12729f899f71e1f438d69015ba18f4f6b441778
magujun <guimaraesjunior.marcelo@gmail.com>
2021-06-18 15:16:24 +0200

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,68 @@
-import React from "react";
-import { useSession, signIn, signOut } from "next-auth/client";
-import Map from "../components/map";
-import StreetMap from "../components/streetmap";
-import { GetStaticProps } from "next";
-import { GameProvider } from "../context/gameContext";
+import React from 'react';
+import {
+	useSession,
+	signIn,
+	signOut,
+} from 'next-auth/client';
+import Map from '../components/map';
+import StreetMap from '../components/streetmap';
+import { GetStaticProps } from 'next';
+import { GameProvider } from '../context/gameContext';
 
 export function Login() {
-  const [session, loading] = useSession();
+	const [session, loading] = useSession();
 
-  // console.log(session);
+	// console.log(session);
 
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn("google")}>Sign in</button>
-    </>
-  );
+	if (session) {
+		return (
+			<>
+				Signed in as {session.user.email} <br />
+				<button onClick={() => signOut()}>Sign out</button>
+			</>
+		);
+	}
+	return (
+		<>
+			Not signed in <br />
+			<button onClick={() => signIn('google')}>
+				Sign in
+			</button>
+		</>
+	);
 }
 
 type HomeProps = {
-  googleMapsApiKey: string;
+	googleMapsApiKey: string;
 };
 
-export default function Home({ googleMapsApiKey }: HomeProps) {
-  return (
-    <GameProvider>
-      <>
-        <Login />
-        <Map googleMapsApiKey={googleMapsApiKey} />
-        <StreetMap googleMapsApiKey={googleMapsApiKey} />
-      </>
-    </GameProvider>
-  );
+export default function Home({
+	googleMapsApiKey,
+}: HomeProps) {
+	return (
+		<GameProvider>
+			<>
+				<Login />
+				<Map googleMapsApiKey={googleMapsApiKey} />
+				<StreetMap googleMapsApiKey={googleMapsApiKey} />
+			</>
+		</GameProvider>
+	);
 }
 
+const removeUndefinedForNextJsSerializing = <T,>(
+	props: T,
+): T =>
+	Object.fromEntries(
+		Object.entries(props).filter(
+			([, value]) => value !== undefined,
+		),
+	) as T;
+
 export const getStaticProps: GetStaticProps = async () => {
-  return {
-    props: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-    },
-  };
+	return {
+		props: removeUndefinedForNextJsSerializing({
+			googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+		}),
+	};
 };
